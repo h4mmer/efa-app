12d46ffa3ecf8f26f305be065d0efaf850890e91
l.h4mmer <l.h4mmer@googlemail.com>
2013-08-04 18:31:37 +0000

    Es funktioniert immer noch nicht

--- EFA/src/com/example/efa/logic/Curl.java
+++ EFA/src/com/example/efa/logic/Curl.java
@@ -24,32 +24,6 @@ import android.os.Build;
 import android.os.StrictMode;
 
 public class Curl {
-	private LogMe log;
-
-	private String log_valid_ids(String s) throws UnsupportedEncodingException,
-			IOException {
-		BufferedReader reader = get_url("https://graph.facebook.com/" + s
-				+ "?fields=id&method=GET");
-		String line;
-		while ((line = reader.readLine()) != null) {
-
-			if (line.contains("id")) {
-				s = line.substring(7, line.lastIndexOf('"'));
-				System.out.println("ID: " + s);
-			} else {
-				s = null;
-			}
-		}
-		return s;
-	}
-
-	private BufferedReader get_url(String s)
-			throws UnsupportedEncodingException, IOException {
-		URL url = new URL(s);
-		BufferedReader reader = new BufferedReader(new InputStreamReader(
-				url.openStream(), "UTF-8"));
-		return reader;
-	}
 
 	/**
 	 * 
@@ -75,13 +49,14 @@ public class Curl {
 	 */
 	@SuppressLint("NewApi")
 	@TargetApi(Build.VERSION_CODES.GINGERBREAD)
-	public void post(String boatName,String boatTyp, String cox, LinkedList<String> crew,
-			String drummer, String date, String startTime, String arivalTime,
-			String route, int distance, String tourType, String comment)
-			throws IOException {
-		
-		String post = "date=1375480155&mail=&dfBoot=" + boatName + "&dfSteuermann=" + cox;
-		
+	public static void post(String boatName, String boatTyp, String cox,
+			LinkedList<String> crew, String drummer, String date,
+			String startTime, String arivalTime, String route, int distance,
+			String tourType, String comment) throws IOException {
+
+		String post = "date=1375480155&mail=&dfBoot=" + boatName
+				+ "&dfSteuermann=" + cox;
+
 		for (String s : crew) {
 			post += "&dfName" + crew.indexOf(s) + 1 + "=" + s;
 		}
@@ -89,7 +64,6 @@ public class Curl {
 				+ "&dfStartZeit=" + startTime + "&dfEndeZeit=" + arivalTime
 				+ "&dfStrecke=" + route + "&dfkm=" + distance + "&cmbFahrtart="
 				+ tourType + "&dfBemerkung=" + comment;
-		post = "date=1375480155&mail=&dfBoot=Test&cmbBootTyp=K1&dfSteuermann=&dfName1=test&dfName2=&dfName3=&dfName4=&dfName5=&dfName6=&dfName7=&dfName8=&dfName9=&dfName10=&dfName11=&dfName12=&dfName13=&dfName14=&dfName15=&dfName16=&dfName17=&dfName18=&dfName19=&dfName20=&dfName21=&dfName22=&dfName23=&dfName24=&cmbTrommler=&dfDatum=0001-01-01&dfStartZeit=02%3A02&dfEndeZeit=23%3A03&dfStrecke=sa&dfkm=0&cmbFahrtart=Wanderfahrt&dfBemerkung=";
 		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder()
 				.permitAll().build();
 		StrictMode.setThreadPolicy(policy);
@@ -98,51 +72,29 @@ public class Curl {
 		// URL url = new URL("http://127.0.0.1/index.php");
 		HttpURLConnection connection = (HttpURLConnection) url.openConnection();
 		try {
-			  connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
-	            connection.setRequestProperty("charset", "utf-8");
-	            connection.setRequestProperty("Content-Length", "" + Integer.toString(post.toString().getBytes().length));
-	            connection.setRequestMethod("POST");
-	            connection.setDoOutput(true);
-	            connection.connect();
-	            DataOutputStream out = new DataOutputStream(connection.getOutputStream());
-	            out.writeBytes(post.toString());
-	            out.flush();
-	            out.close();
-	            InputStream is = connection.getInputStream();
-	            InputStreamReader reader = new InputStreamReader(is);
-	            BufferedReader r = new BufferedReader(reader);
-	              StringBuilder total = new StringBuilder();
-	              String line;
-	              while ((line = r.readLine()) != null) {
-	                  total.append(line);
-	              }
+			connection.setRequestProperty("Content-Type",
+					"application/x-www-form-urlencoded");
+			connection.setRequestProperty("charset", "utf-8");
+			connection.setRequestProperty("Content-Length",
+					"" + Integer.toString(post.toString().getBytes().length));
+			connection.setRequestMethod("POST");
+			connection.setDoOutput(true);
+			connection.connect();
+			DataOutputStream out = new DataOutputStream(
+					connection.getOutputStream());
+			out.writeBytes(post.toString());
+			out.flush();
+			out.close();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					url.openStream(), "UTF-8"));
+			StringBuilder total = new StringBuilder();
+			String line;
+			while ((line = reader.readLine()) != null) {
+				total.append(line);
+			}
 
-	              
 		} finally {
 			connection.disconnect();
 		}
-		// connection.setRequestMethod("POST");
-		// connection.setDoInput(true);
-		// connection.setDoOutput(true);
-		// connection.setUseCaches(false);
-		// connection.setRequestProperty("Content-Type",
-		// "application/x-www-form-urlencoded");
-		// connection.setRequestProperty("Content-Length",
-		// String.valueOf(post.length()));
-		//
-		// OutputStreamWriter writer = new OutputStreamWriter(
-		// connection.getOutputStream());
-		// writer.write(post);
-		// writer.flush();
-		//
-		// BufferedReader reader = new BufferedReader(new InputStreamReader(
-		// connection.getInputStream()));
-		// String line;
-		// while ((line = reader.readLine()) != null) {
-		// System.out.println(line);
-		// }
-		//
-		// writer.close();
-		// reader.close();
 	}
 }
